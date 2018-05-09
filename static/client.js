26b90378dfc813f8d90cba269097c7a73b59b95b
oleg092 <xxxx092@yandex.ru>
2018-05-09 19:46:49 +0400

    add reg page

--- static/client.js
+++ static/client.js
@@ -1,1 +1,11 @@
 /*this file for client code on javascript*/
+function checkParams() {
+    var email = $('#email').val();
+    var pas = $('#pas').val();
+
+    if(email.length != 0 && pas.length != 0) {
+        $('#buttonSignIn').removeAttr('disabled');
+    } else {
+        $('#buttonSignIn').attr('disabled', 'disabled');
+    }
+}
