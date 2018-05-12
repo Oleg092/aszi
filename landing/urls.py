7e665170fd828b2821c19423b7872ef8c4cc9ad6
oleg092 <xxxx092@yandex.ru>
2018-05-12 17:44:15 +0400

    ligt version logic for registration and write message about failed autorization for users

--- landing/urls.py
+++ landing/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
 from landing import views
+from landing.views import Autarization
+
 
 urlpatterns = [
-    url(r'^landing/', views.landing, name='landing'),
+    url(r'^landing/', Autarization.as_view()),
 
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView, DetailView, ListView
+from django.views.generic.base import RedirectView
+from django.urls import reverse
+import json
+from django.contrib import messages
 
-def landing(request):
-    return render(request, 'landing/landing.html', locals())
+
+class Autarization(TemplateView):
+    template_name = 'landing/landing.html'
+
+    def post(self, request):
+        email = request.POST['email']
+        pas = request.POST['pas']
+        if email == "xxxx092@ya.ru" and pas == "123123":
+            return HttpResponseRedirect('/landing')
+        else:
+            messages.success(request, "incorrect password or email")
+            context = HttpResponseRedirect(request.path)
+            return context
+
+
+
