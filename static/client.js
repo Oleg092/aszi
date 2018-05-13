93b5ad41a50aeb0cc32cfaa7377628a5f4f95878
vodeolay <kulakovma18@gmail.com>
2018-05-13 19:39:24 +0400

    Modal Registration window

--- static/client.js
+++ static/client.js
@@ -1,11 +1,3 @@
-/*this file for client code on javascript*/
-function checkParams() {
-    var email = $('#email').val();
-    var pas = $('#pas').val();
-
-    if(email.length != 0 && pas.length != 0) {
-        $('#buttonSignIn').removeAttr('disabled');
-    } else {
-        $('#buttonSignIn').attr('disabled', 'disabled');
-    }
-}
+$("#regButton").click(function() {
+    $("#myModalBox").modal('show');
+});
