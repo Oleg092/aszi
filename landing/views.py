dd0faf9e139b7f5f62191cf11238da5198adccb6
oleg092 <xxxx092@yandex.ru>
2018-05-19 18:47:29 +0400

    reg: add users and check unic email

--- landing/views.py
+++ landing/views.py
@@ -1,12 +1,8 @@
-from django.http import HttpResponse
-from django.http import HttpResponseBadRequest
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.template import RequestContext
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, FormView
-import json
 from django.contrib import messages
-
+from landing.models import Users
 from landing.forms import UsersForm
 
 
@@ -24,23 +20,32 @@ class Autorization(TemplateView, FormView):
                 messages.success(request, "incorrect password or email")
                 context = HttpResponseRedirect(request.path)
                 context["data"] = 'auth_failed'
-                #self.requestclient(request)
+                # self.requestclient(request)
                 return context
         except:
             print("not autorization")
-        return self.requestclient(request)
+            return self.form_valid(request)
 
     def form_valid(self, form):
         print('vizov')
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        self.requestclient(form)
-        ###form.save()
-        #return super().form_valid(form)
+        form1 = UsersForm(form.POST)
+        if form1.is_valid():
+            try:
+                get_object_or_404(Users, email=form.POST["email"])
+            except:
+                form1.save()
+                messages.success(form, "reg_success")
+                context = HttpResponseRedirect('/landing')
+                return context
+
+        return self.requestclient(form)
+        # return super().form_valid(form)
 
     def requestclient(self, request):
         print("wezdes")
-        messages.success(request, "reg")
+        messages.success(request, "reg_failed")
         context = HttpResponseRedirect('/landing')
         context["data"] = 'auth_failed'
         return context
