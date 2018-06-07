d51af1b0de45090bcc5101cd48079017f9792a49
oleg <xxxx092@yandex.ru>
2018-06-07 22:42:28 +0400

    cookie logout in class user cookie

--- landing/UserCookie.py
+++ landing/UserCookie.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 
 
-class UserCookie(object):
+class UserCookie(object):  # класс для работы с cookie пользователя
     @staticmethod
     def setCookie(user):
         context = HttpResponseRedirect("/home")
@@ -11,4 +11,15 @@ class UserCookie(object):
         context.set_cookie("lastName", user.lastname)
         context.set_cookie("is_admin", user.is_admin)
         context.set_cookie("is_active", user.is_active)
+        return context
+    
+    @staticmethod
+    def delCookie():
+        context = HttpResponseRedirect("/landing")
+        context.set_cookie("session", "false")
+        context.set_cookie("user", "none")
+        context.set_cookie("userName", "none")
+        context.set_cookie("lastName", "none")
+        context.set_cookie("is_admin", "none")
+        context.set_cookie("is_active", "none")
         return context
--- landing/logout.py
+++ landing/logout.py
@@ -1,17 +1,11 @@
 from django.views.generic import TemplateView
-from django.http import HttpResponseRedirect
+from landing.UserCookie import UserCookie
 
 
 class LogOut(TemplateView):
     template_name = "landing/LogOut.html"
 
-    #после нажатия кнопки logout обнуляются куки и пользователь редиректится на страницу авторизации
+    # после нажатия кнопки logout обнуляются куки и пользователь редиректится на страницу авторизации
     def post(self, *args):
-        context = HttpResponseRedirect("/landing")
-        context.set_cookie("session", "false")
-        context.set_cookie("user", "none")
-        context.set_cookie("userName", "none")
-        context.set_cookie("lastName", "none")
-        context.set_cookie("is_admin", "none")
-        context.set_cookie("is_active", "none")
+        context = UserCookie.delCookie()
         return context
